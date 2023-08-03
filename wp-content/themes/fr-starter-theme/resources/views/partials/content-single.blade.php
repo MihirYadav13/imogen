9a6fbad3e5879ff78f572c872684b6adc2a12a2b
shaheenza <shaheen@freerangestudios.com>
2023-08-03 16:13:16 +0530

    Feature/IF-123:: Post Detail View added with social media share component.

--- wp-content/themes/fr-starter-theme/resources/views/partials/content-single.blade.php
+++ wp-content/themes/fr-starter-theme/resources/views/partials/content-single.blade.php
@@ -1,19 +1,21 @@
+<div class="container">
 <article @php(post_class())>
   <header>
-    <h1 class="entry-title">
+    <h3 class="entry-title">
       {!! $title !!}
-    </h1>
-
-    @include('partials.entry-meta')
+    </h3>   
   </header>
-
+  <div class="post-meta-data">
+    @include('partials.entry-meta')
+    <x-share-button />
+  </div>
   <div class="entry-content">
     @php(the_content())
   </div>
-
+  @include('partials.entry-author-meta')
   <footer>
     {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
   </footer>
 
-  @php(comments_template())
 </article>
+</div>
