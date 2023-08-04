03c6b29bd215427c1c3a1ff933dfe9b40616b317
TanmayFR <tanmay@freerange.com>
2023-08-04 15:32:39 +0530

    IF-69 svg fixed

--- wp-content/themes/fr-starter-theme/resources/views/blocks/hero.blade.php
+++ wp-content/themes/fr-starter-theme/resources/views/blocks/hero.blade.php
@@ -1,26 +1,20 @@
 <div class="fr-hero hero {{ $block->classes }}">
-	@include('components.responsive-acf-image', ['image' => ['url' => 'https://picsum.photos/seed/picsum/1280/720'], 'class' => 'hero-bg-image desktop'])
-	@include('components.responsive-acf-image', ['image' => $background_image_mobile, 'class' => 'hero-bg-image mobile'])
-	<div class="content-container">
-		<div class="content-background">
-			<div class="waveWrapper waveAnimation">
-				<div class="waveWrapperInner bgTop">
-				<div class="wave waveTop"></div>
-				</div>
-				<div class="waveWrapperInner bgMiddle">
-				<div class="wave waveMiddle"></div>
-				</div>
-				<div class="waveWrapperInner bgBottom">
-				<div class="wave waveBottom"></div>
-				</div>
-			</div>
-		</div>
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-6 content-col wysiwyg-content">
-					{!! $content !!}
-				</div>
-			</div>
-		</div>
-	</div>
+    @include('components.responsive-acf-image', [
+        'image' => $background_image,
+        'class' => 'hero-bg-image desktop',
+    ])
+    @include('components.responsive-acf-image', [
+        'image' => $background_image_mobile,
+        'class' => 'hero-bg-image mobile',
+    ])
+
+    <div class="content-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 content-col wysiwyg-content">
+                    {!! $content !!}
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
