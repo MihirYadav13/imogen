7d9925ce8d94f4c530e4c03a4d95fc3b8f9726d2
shaheenza <shaheen@freerangestudios.com>
2023-08-16 19:46:41 +0530

    issues/IF-123:: Author selection box added to post detail page.

--- wp-content/themes/fr-starter-theme/resources/views/partials/entry-author-meta.blade.php
+++ wp-content/themes/fr-starter-theme/resources/views/partials/entry-author-meta.blade.php
@@ -1,15 +1,17 @@
-@if(get_the_author_meta('display_name') != '')
+@php
+$user = get_field('select_author', get_the_ID());
+@endphp
+@if($user)
   <div class="hr"></div>
-  <div class="byline author vcard" >
-    
+  <div class="byline author vcard" >    
     <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">    
-      {!! get_avatar(get_the_author_meta('ID'), 80) !!}
+      {!! get_avatar(get_the_author_meta('ID', $user), 80) !!}
       </a>
       <div class="auth-detail">
       <h5>
-        {{ get_the_author_meta('first_name')}} &nbsp;{{ get_the_author_meta('last_name')}}</h5>
+        {{ get_the_author_meta('first_name', $user)}} &nbsp;{{ get_the_author_meta('last_name', $user)}}</h5>
         <p>
-        {!! get_the_author_meta('description') !!}
+        {!! get_the_author_meta('description', $user) !!}
         </p>
     </div> 
   </div>
