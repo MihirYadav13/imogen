30c1c3a356d9edf7fabf0766fadc905f38664f8c
Dhananjay <dhananjay@sorigin.com>
2023-08-18 13:53:08 +0530

    Changed contact us css

--- wp-content/themes/fr-starter-theme/app/lib/Cf7Validation.php
+++ wp-content/themes/fr-starter-theme/app/lib/Cf7Validation.php
@@ -9,30 +9,31 @@ if (!class_exists('Cf7Validation')) :
             // Other constructor code...
 
             // Add filter to validate CF7 text fields for blank spaces
-            add_filter('wpcf7_validate_text*', [$this, 'validateCF7TextField'], 10, 2);
-            add_filter('wpcf7_validate_textarea*', [$this, 'validateCF7TextField'], 10, 2); // Add this line for textarea fields
+            add_filter('wpcf7_validate_text*', [$this, 'validateCF7RequiredField'], 10, 2);
+            add_filter('wpcf7_validate_textarea*', [$this, 'validateCF7RequiredField'], 10, 2); // Add this line for textarea fields
         }
 
         // Other methods...
 
         // Validate CF7 text fields for blank spaces
-        public function validateCF7TextField($result, $tag)
+        public function validateCF7RequiredField($result, $tag)
         {
-            $field_names_to_validate = ['first_name', 'last_name', 'message']; // Add 'Message' here
-
             // Check if the field name matches the text fields you want to validate
-            if (in_array($tag['name'], $field_names_to_validate)) {
-                $value = isset($_POST[$tag['name']]) ? trim($_POST[$tag['name']]) : '';
+            $value = isset($_POST[$tag['name']]) ? trim($_POST[$tag['name']]) : '';
 
-                // Check if the value contains only blank spaces
-                if (empty($value) || strlen($value) === 0) {
-                    $result->invalidate($tag, "Blank spaces are not allowed.");
-                }
+            // Check if the value contains only blank spaces
+            if (empty($value) || strlen($value) === 0) {
+                $result->invalidate($tag, "Blank spaces are not allowed.");
+            }
 
-                // Check if the value contains only numbers or only special characters
-                if ($tag['name'] !== 'message' && (ctype_digit($value) || preg_match('/^[[:punct:]]+$/', $value))) {
-                    $result->invalidate($tag, "Numbers and special characters are not allowed.");
-                }
+            // Check if the value contains only numbers or only special characters
+            if (($tag['name'] === 'first_name' || $tag['name'] === 'last_name') && !ctype_alnum($value)) {
+                $result->invalidate($tag, "Enter only alphanumeric characters.");
+            }
+
+            // Check if the value contains only numbers
+            if (($tag['name'] === 'first_name' || $tag['name'] === 'last_name') && ctype_digit($value)) {
+                $result->invalidate($tag, "Only numbers are not allowed.");
             }
 
             return $result;
