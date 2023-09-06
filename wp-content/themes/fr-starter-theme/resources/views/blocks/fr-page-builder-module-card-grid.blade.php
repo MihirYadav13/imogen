8016487e2ecf2d424d0d9c42b5861fb98974abc0
TanmayFR <tanmay@freerange.com>
2023-09-06 15:25:11 +0530

    bugfix: Fixed blade structure.

--- wp-content/themes/fr-starter-theme/resources/views/blocks/fr-page-builder-module-card-grid.blade.php
+++ wp-content/themes/fr-starter-theme/resources/views/blocks/fr-page-builder-module-card-grid.blade.php
@@ -1,11 +1,11 @@
 <div class="module card-grid-with-filter-module {{ $block->classes }}"
     @if (isset($block->block->anchor)) id="{{ $block->block->anchor }}" @endif>
-    @if (in_array('post', $blockData['post_type']))
-        <div class="container-fluid">
-            <div class="col wysiwyg-heading-content wysiwyg-content">
-                {!! $heading_content !!}
-            </div>
+    <div class="container-fluid">
+        <div class="col wysiwyg-heading-content wysiwyg-content">
+            {!! $heading_content !!}
         </div>
+    </div>
+    @if (in_array('post', $blockData['post_type']))
         <x-card-grid-filter :filter-id="$filterId" :filters="$frontendFilters" :includeSearch="true" :block-data="$blockData" />
     @endif
     <x-card-grid :posts-per-page="$postsPerPage" :connected-filters="[$filterId]" :load-more-text="$loadMoreText" :block-data="$blockData" :show-no-result="in_array('post', $blockData['post_type'])" />
