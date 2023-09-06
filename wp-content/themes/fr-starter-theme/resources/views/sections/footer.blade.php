c2cceabf6c66c031364dab7f5ce4b758d2ebdd8c
shaheenza <shaheen@freerangestudios.com>
2023-09-06 13:01:49 +0530

    issues/IF-252: Footer wave animation cod eupdated.

--- wp-content/themes/fr-starter-theme/resources/views/sections/footer.blade.php
+++ wp-content/themes/fr-starter-theme/resources/views/sections/footer.blade.php
@@ -3,15 +3,7 @@
 @endif
 <footer class="content-info">
   <div class="waveWrapper waveAnimation">
-    <div class="waveWrapperInner bgTop">
-      <div class="wave waveTop"></div>
-    </div>
-    <div class="waveWrapperInner bgMiddle">
-      <div class="wave waveMiddle"></div>
-    </div>
-    <div class="waveWrapperInner bgBottom">
-      <div class="wave waveBottom"></div>
-    </div>
+  <div class="wave"></div>
   </div>
     <div class="container">
         <div class="footer-header">
