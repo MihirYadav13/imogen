aa7423c038f47cace3bb3a1de43d6fb585d173f4
Dhananjay <dhananjay@sorigin.com>
2023-09-15 11:21:19 +0530

    Removed unused code

--- wp-content/themes/fr-starter-theme/resources/views/blocks/fr-page-builder-module-related-content-card-grid.blade.php
+++ wp-content/themes/fr-starter-theme/resources/views/blocks/fr-page-builder-module-related-content-card-grid.blade.php
@@ -1,8 +1,7 @@
-<div class="module related-content-card-grid-with-filter-module {{ $block->classes }}" @if (isset($block->block->anchor))
-    id="{{ $block->block->anchor }}"
-    @endif>
+<div class="module related-content-card-grid-with-filter-module {{ $block->classes }}"
+    @if (isset($block->block->anchor)) id="{{ $block->block->anchor }}" @endif>
     @include('components.grid-heading-content')
-    <x-card-grid :posts-per-page="$postsPerPage" :connected-filters="[$filterId]" :load-more-text="$loadMoreText" :load-more-url="$loadMoreUrl" :block-data="$blockData" />
+    <x-card-grid :posts-per-page="$postsPerPage" :connected-filters="[$filterId]" :load-more-text="$loadMoreText" :block-data="$blockData" />
     <div class="redirect-button">
         <x-cta-button :label="$loadMoreText" type="external_url" style="secondary" :external-url="$loadMoreUrl" :new-tab="false" />
     </div>
