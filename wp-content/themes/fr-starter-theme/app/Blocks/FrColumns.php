6f522dca2d6216fa7012fcfc180a650d1bc6418b
TanmayFR <tanmay@freerange.com>
2023-09-20 15:47:05 +0530

    Added bottom spacing option for FR columns module.

--- wp-content/themes/fr-starter-theme/app/Blocks/FrColumns.php
+++ wp-content/themes/fr-starter-theme/app/Blocks/FrColumns.php
@@ -21,12 +21,12 @@ class FrColumns extends Block
      */
     public $description = 'A simple Free Range Columns block.';
 
-	/**
-	 * The block slug.
-	 *
-	 * @var string
-	 */
-	public $slug = 'fr-page-builder-module-free-range-columns';
+    /**
+     * The block slug.
+     *
+     * @var string
+     */
+    public $slug = 'fr-page-builder-module-free-range-columns';
 
     /**
      * The block category.
@@ -125,7 +125,9 @@ class FrColumns extends Block
             'is_flipped' => $items['is_flipped'],
             'choices' => $items['choices'],
             'max_width' => $this->getMaxWidthAttr(),
-            'allowed_blocks' => json_encode($allowed_blocks)
+            'allowed_blocks' => json_encode($allowed_blocks),
+            'bottom_spacing' => $this->preview && !get_field('bottom_spacing') ? $this->example['bottom_spacing'] : get_field('bottom_spacing'),
+
         ];
     }
 
@@ -148,14 +150,30 @@ class FrColumns extends Block
                 ]
             ])
             ->addTrueFalse('is_flipped')
-                ->conditional('layouts', '==', '13_23')
-                ->or('layouts', '==', '14_34')
+            ->conditional('layouts', '==', '13_23')
+            ->or('layouts', '==', '14_34')
             ->addRange('max_width', [
                 'label' => 'Layout\'s Max Width',
                 'min' => 10,
                 'max' => 100,
                 'append' => '%',
                 'default_value' => 100
+            ])
+            ->addSelect('bottom_spacing', [
+                'choices' => [
+                    'default' => 'default',
+                    'zero-space' => 'zero',
+                    'heading-space' => 'heading',
+                    'module-space' => 'module',
+                ],
+                'default_value' => [],
+                'ui' => 0,
+                'return_format' => 'both',
+                'placeholder' => '',
+                'wrapper' => [
+                    'width' => '30'
+                ],
+                'required' => '1'
             ]);
 
         return $frLayout->build();
@@ -170,12 +188,13 @@ class FrColumns extends Block
     {
         return [
             'layouts' => get_field('layouts'),
-            'is_flipped' => get_field('is_flipped')?:false,
+            'is_flipped' => get_field('is_flipped') ?: false,
             'choices' => $this->choices(['remove_first' => true]),
         ];
     }
 
-    public function choices($args = []) {
+    public function choices($args = [])
+    {
         $choices = [];
         $choices['-1'] = 'None';
         $choices['1_1'] = '1/1';
@@ -183,18 +202,19 @@ class FrColumns extends Block
         $choices['13_13_13'] = '1/3 1/3 1/3';
         $choices['13_23'] = '1/3 2/3';
         $choices['14_34'] = '1/4 3/4';
-        
-        if(isset($args['remove_first']) && $args['remove_first']){
+
+        if (isset($args['remove_first']) && $args['remove_first']) {
             unset($choices['-1']);
         }
 
         return $choices;
     }
 
-    public function getMaxWidthAttr(){
+    public function getMaxWidthAttr()
+    {
         $max_width = get_field('max_width');
-		return $max_width && $max_width !== 100 ? 'style=\'max-width:'.$max_width.'%;\'' : '' ;
-	}
+        return $max_width && $max_width !== 100 ? 'style=\'max-width:' . $max_width . '%;\'' : '';
+    }
 
     /**
      * Assets to be enqueued when rendering the block.
